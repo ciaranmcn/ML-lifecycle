143afff81653868df3bb2463b4d6e163814ccaa6
ciaranclm <ciaranmcneely@gmial.com>
2025-06-01 00:37:09 -0700

    Added to feedback workflow

--- app/app.py
+++ app/app.py
@@ -13,16 +13,45 @@ from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from app.telemetry import setup_telemetry
 
 app = FastAPI()
+tracer = setup_telemetry()
+FastAPIInstrumentor.instrument_app(app)
 temporal_client = None  
+
 
 @app.on_event("startup")
 async def startup_event():
     global temporal_client
     temporal_client = await Client.connect("localhost:7233")
 
-@app.get("/heartbeat/{connector_id}")
-def heartbeat(connector_id: str):
-    return {"status": "ok", "id": connector_id}
+@app.post("/start")
+async def start_workflow():
+    with tracer.start_as_current_span("start_workflow"):
+        workflow_id = str(uuid.uuid4())
+        await temporal_client.start_workflow(
+            FeedbackWorkflow.run,
+            id=workflow_id,
+            task_queue="feedback-task-queue"
+        )
+        return {"workflow-id": workflow_id}
+
+@app.post("/send")
+async def send_feedback(request: Request):
+    with tracer.start_as_current_span("send_feedback"):
+        body = await request.json()
+        workflow_id = body["workflow_id"]
+        feedback = body["message"]
+
+        handle = temporal_client.get_workflow_handle(workflow_id)
+        await handle.signal("feedback", feedback)
+        return {"status": "signal sent", "workflow-id": workflow_id}
+
+@app.get("/result/{workflow_id}")
+async def get_result(workflow_id: str):
+    with tracer.start_as_current_span("get_result"):
+        handle = temporal_client.get_workflow_handle(workflow_id)
+        result = await handle.result()
+        return {"result": result}
+
 
 class InputData(BaseModel):
     feature1: float
@@ -47,29 +76,3 @@ def train(config: FullTrainConfig):
         "status": result, 
         "preprocessed_file": processed_path
     }
-
-@app.post("/start")
-async def start_workflow():
-    workflow_id = str(uuid.uuid4())
-    await temporal_client.start_workflow(
-        FeedbackWorkflow.run,
-        id=workflow_id,
-        task_queue="feedback-task-queue"
-    )
-    return {"workflow-id": workflow_id}
-
-@app.post("/send")
-async def send_feedback(request: Request):
-    body = await request.json()
-    workflow_id = body["workflow_id"]
-    feedback = body["message"]
-
-    handle = temporal_client.get_workflow_handle(workflow_id)
-    await handle.signal("feedback", feedback)
-    return {"status": "signal sent", "workflow-id": workflow_id}
-
-@app.get("/result/{workflow_id}")
-async def get_result(workflow_id: str):
-    handle = temporal_client.get_workflow_handle(workflow_id)
-    result = await handle.result()
-    return {"result": result}
--- app/workflows.py
+++ app/workflows.py
@@ -1,4 +1,6 @@
 from temporalio import workflow
+from opentelemetry import trace 
+tracer = trace.get_tracer(__name__)
 
 @workflow.defn
 class FeedbackWorkflow:
@@ -6,12 +8,30 @@ class FeedbackWorkflow:
         self.feedback_msg = None
         
     @workflow.signal
-    async def feedback(self, value: str):
-        workflow.logger.info(f"Signal recieved: {value}")
-        self.feedback_msg = value
+    async def feedback(self, msg: str):
+        workflow.logger.info(f"Signal recieved: {msg}")
+        self.feedback_msg = msg
 
+    @workflow.signal
+    async def cancel(self):
+        self._cancelled = True
+        
+    @workflow.signal
+    async def update_feedback(self, new_msg):
+        self.feedback_msg =f"(Updated) {new_msg}"
+        
     @workflow.run
     async def run(self) -> str:
-        await workflow.wait_condition(lambda: self.feedback_msg is not None)
-        return f"Got feedback: {self.feedback_msg}"
+        with tracer.start_as_current_span("workflow_run"):
+            workflow.logger.info("Waiting for feedback...")
+            await workflow.wait_condition(lambda: self.feedback_msg is not None or self._cancelled)
+            
+            if self._cancelled:
+                return "Workflow was cancelled"
+
+            workflow.logger.info(f"Got feedback: {self.feedback_msg}")
+            
+            return f"Final response: {self.feedback_msg}"
+
+
 
